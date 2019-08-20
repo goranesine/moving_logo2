8621cda4ee1ebaaacfe15d2a949d93bbf6ebe061
goranesine <goranesine@gmail.com>
2019-08-20 21:08:58 +0200

    muving logo bez fireclauda

--- lib/moving_container_page.dart
+++ lib/moving_container_page.dart
@@ -1,22 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
-class Record {
-  final int osX;
-  final int osY;
-  final DocumentReference reference;
-
-  Record.fromMap(Map<String, dynamic> map, {this.reference})
-      : assert(map['osx'] != null),
-        assert(map['osy'] != null),
-        osX = map['osx'],
-        osY = map['osy'];
-
-  Record.fromSnapshot(DocumentSnapshot snapshot)
-      : this.fromMap(snapshot.data, reference: snapshot.reference);
+import 'dart:math';
 
 
-}
 
 class MovingContainer extends StatefulWidget{
   @override
@@ -24,65 +10,38 @@ class MovingContainer extends StatefulWidget{
 }
 
 class _MovingContainerState extends State<MovingContainer> {
-  int osX;
-  int osY;
 
+    Random _slucajanBroj = Random();
+  double osX = 0.0;
+  double osY = 0.0;
+
+    _noviOffset(){
+      osX = _slucajanBroj.nextDouble();
+      osY = _slucajanBroj.nextDouble();
+      setState(() {
+      });
+    }
   @override
+
+
+
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: Text("Moving Container"),),
-          body: _buildBody(context),
+      appBar: AppBar(title: Text("Moving Logo"),),
+          body:
+          Container(
+            color: Colors.amber,
+            child: GestureDetector(
+              onTap: _noviOffset,
+              child: FlutterLogo(
+                size: 200,
+              ),
+            ),
+            alignment: Alignment(osX, osY),
+          ),
     );
   }
-}
-
-Widget _buildBody(BuildContext context) {
-  return StreamBuilder<QuerySnapshot>(
-    stream: Firestore.instance.collection('baby').snapshots(),
-    builder: (context, snapshot) {
-      if (!snapshot.hasData) return LinearProgressIndicator();
-
-      return _buildList(context, snapshot.data.documents);
-    },
-  );
-}
-
-Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
-  return ListView(
-    padding: const EdgeInsets.only(top: 20.0),
-    children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-  );
-}
-
-Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-  final record = Record.fromSnapshot(data);
-
-  return Padding(
-    key: ValueKey(record.osX),
-    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-    child: Container(
-
-      width: (record.osX.toDouble()),
-      height: (record.osY.toDouble()),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(5.0),
-      ),
-      child: ListTile(
-        title: Text(record.osX.toString()),
-        trailing: Text(record.osY.toString()),
-        onTap: () =>
-            Firestore.instance.runTransaction((transaction) async {
-              final freshSnapshot = await transaction.get(record.reference);
-              final fresh = Record.fromSnapshot(freshSnapshot);
-
-              await transaction
-                  .update(record.reference, {'osx': fresh.osX + 1, "osy" : fresh.osY - 1});
-            }),
-      ),
-    ),
-  );
 }
 
 
