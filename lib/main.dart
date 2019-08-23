f8db6ca56e34ae0b9117041e9a08cc97b0914ab1
goranesine <goranesine@gmail.com>
2019-08-23 22:53:40 +0200

    Moving_logo povezan sa Firestorom radi

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'moving_container_page.dart';
+import 'dart:math';
 
 void main() => runApp(MyApp());
 
-
+void NoveKoordinate() {
+  Random _slucajanBroj = Random();
+  double osX = -0.5 +_slucajanBroj.nextDouble();
+  double osY = -0.5 +_slucajanBroj.nextDouble();
+  Firestore.instance
+      .collection("baby")
+      .document("values")
+      .updateData({"osx": osX, "osy": osY});
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -16,82 +24,32 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MovingContainer extends StatefulWidget {
   @override
-  _MyHomePageState createState() {
-    return _MyHomePageState();
-  }
+  _MovingContainerState createState() => _MovingContainerState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MovingContainerState extends State<MovingContainer> {
+  final db = Firestore.instance;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Moving Container')),
-      body: _buildBody(context),
-    );
+    // TODO: implement build
+    return StreamBuilder<DocumentSnapshot>(
+        stream: db.collection("baby").document("values").snapshots(),
+        builder: (context, snapshot) {
+          Map data = snapshot.data.data;
+
+          return Container(
+            color: Colors.amber,
+            child: GestureDetector(
+              onTap: NoveKoordinate,
+              child: FlutterLogo(
+                size: 200,
+              ),
+            ),
+            alignment: Alignment(data["osx"], data["osy"]),
+          );
+        });
   }
-
-  Widget _buildBody(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('baby').snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) return LinearProgressIndicator();
-
-        return _buildList(context, snapshot.data.documents);
-      },
-    );
-  }
-
-  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
-    return ListView(
-      padding: const EdgeInsets.only(top: 20.0),
-      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-    );
-  }
-
-  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-    final record = Record.fromSnapshot(data);
-
-
-    return Padding(
-      key: ValueKey(record.name),
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(5.0),
-        ),
-        child: ListTile(
-          title: Text(record.name),
-          trailing: Text(record.votes.toString()),
-          onTap: () => Firestore.instance.runTransaction((transaction) async {
-            final freshSnapshot = await transaction.get(record.reference);
-            final fresh = Record.fromSnapshot(freshSnapshot);
-
-            await transaction
-                .update(record.reference, {'votes': fresh.votes + 1});
-          }),
-        ),
-      ),
-    );
-  }
-}
-
-class Record {
-  final String name;
-  final int votes;
-  final DocumentReference reference;
-
-  Record.fromMap(Map<String, dynamic> map, {this.reference})
-      : assert(map['name'] != null),
-        assert(map['votes'] != null),
-        name = map['name'],
-        votes = map['votes'];
-
-  Record.fromSnapshot(DocumentSnapshot snapshot)
-      : this.fromMap(snapshot.data, reference: snapshot.reference);
-
-  @override
-  String toString() => "Record<$name:$votes>";
 }
